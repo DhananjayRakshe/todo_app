9ed35d84176da60eacca4002568601fe7910b40b
Dhananjay Rakshe <47498631+DhananjayRakshe@users.noreply.github.com>
2021-10-13 19:57:10 +0530

    about changes

--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -3,7 +3,7 @@ import {TodoItem} from "./TodoItem";
 
 export const Todos = (props) => {
     let myStyle = {
-        minHeight: "40vh",
+        minHeight: "70vh",
         margin: "40px auto"
     }
     return (
